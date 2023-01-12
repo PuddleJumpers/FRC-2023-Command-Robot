5ead4d4a390e36d8fb2fdf83d118e0449b52acc3
Leo Watt <leobwatt@gmail.com>
2023-01-12 15:03:31 -0800

    more drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,18 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
-}
+    //PWM's for wiring
+    public static final int LEFT_FRONT = 0;
+    public static final int LEFT_BACK = 1;
+    public static final int RIGHT_FRONT = 2;
+    public static final int RIGHT_BACK = 3;
+    public static final int XBOX_LEFT_Y_AXIS = 1;
+    public static final int XBOX_LEFT_X_AXIS = 0;
+    public static final int XBOX_RIGHT_Y_AXIS = 5;
+    public static final int XBOX_RIGHT_X_AXIS = 4;
+    public static final double DRIVETRAINSPEED = 0.7;
+    public static final double DRIVE_FORWARD_TIME = 3.0;
+    public static final double AUTONOMOUS_SPEED = 0.4;
+    public static final int JOYSTICK_NUMBER = 0;
+
+    }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,14 +4,59 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
+
+  Spark leftFront;
+  Spark leftBack;
+  Spark rightFront;
+  Spark rightBack;
+
+  MotorControllerGroup leftMotors;
+  MotorControllerGroup rightMotors;
+
+  DifferentialDrive drive;
+
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+
+    leftFront = new Spark(Constants.LEFT_FRONT);
+    leftBack = new Spark(Constants.LEFT_BACK);
+    rightFront = new Spark(Constants.RIGHT_FRONT);
+    rightBack = new Spark(Constants.RIGHT_BACK);
+
+    leftMotors = new MotorControllerGroup(leftBack, leftFront);
+    rightMotors = new MotorControllerGroup(rightBack, rightFront);
+
+    drive = new DifferentialDrive(leftMotors, rightMotors);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void driveWithController (XboxController controller, double speed) {
+
+    drive.arcadeDrive(controller.getRawAxis(Constants.XBOX_LEFT_Y_AXIS)*speed, controller.getRawAxis(Constants.XBOX_LEFT_X_AXIS)*speed);
+  }
+ 
+   public void driveForward(double speed) {
+
+    drive.tankDrive(speed, speed);
+
+   }
+
+   public void stop() {
+
+    drive.stopMotor();
+
+   }
+
 }
